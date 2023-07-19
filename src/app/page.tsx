6db1ebfbb36ad4991a467fbc18befec5f2775aec
Igor Bedesqui <igorbedesqui@gmail.com>
2023-07-19 15:26:29 +0100

    make form actually work

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,53 @@ const formSchema = z.object({
   hexcode: z.string().min(6).max(6),
 });
 
+/**
+ * @param H : hexcode, expected format: #000 or #000000
+ * @returns HSL values, format: 0deg 0% 0%
+ * @see: https://css-tricks.com/converting-color-spaces-in-javascript/, slighly modified to be typesafe
+ */
+const hexToHSL = (H: string) => {
+  // Convert hex to RGB first
+  let r = 0,
+    g = 0,
+    b = 0;
+  if (H.length == 4) {
+    r = Number('0x' + H[1] + H[1]);
+    g = Number('0x' + H[2] + H[2]);
+    b = Number('0x' + H[3] + H[3]);
+  } else if (H.length == 7) {
+    r = Number('0x' + H[1] + H[2]);
+    g = Number('0x' + H[3] + H[4]);
+    b = Number('0x' + H[5] + H[6]);
+  }
+  // Then to HSL
+  r /= 255;
+  g /= 255;
+  b /= 255;
+  let cmin = Math.min(r, g, b),
+    cmax = Math.max(r, g, b),
+    delta = cmax - cmin,
+    h = 0,
+    s = 0,
+    l = 0;
+
+  if (delta == 0) h = 0;
+  else if (cmax == r) h = ((g - b) / delta) % 6;
+  else if (cmax == g) h = (b - r) / delta + 2;
+  else h = (r - g) / delta + 4;
+
+  h = Math.round(h * 60);
+
+  if (h < 0) h += 360;
+
+  l = (cmax + cmin) / 2;
+  s = delta == 0 ? 0 : delta / (1 - Math.abs(2 * l - 1));
+  s = +(s * 100).toFixed(1);
+  l = +(l * 100).toFixed(1);
+
+  return `${h}deg ${s}% ${l}%`;
+};
+
 export default function Home() {
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
@@ -31,7 +78,9 @@ export default function Home() {
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
-    console.log(values);
+    console.log(
+      `hexcode: ${values.hexcode}, hsl: ${hexToHSL(`#${values.hexcode}`)}`
+    );
   }
   return (
     <main className="dark grid place-items-center min-h-screen bg-background text-foreground">
@@ -60,7 +109,7 @@ export default function Home() {
               </FormItem>
             )}
           />
-          <Button type="submit">Submit</Button>
+          <Button type="submit">Copy result to clipboard</Button>
         </form>
       </Form>
     </main>
